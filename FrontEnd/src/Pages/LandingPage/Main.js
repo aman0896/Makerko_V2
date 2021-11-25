4629138bc58ee6401ef582fb0cab283088f14194
aman0896 <aman.promech@gmail.com>
2021-11-25 15:52:34 +0545

    Main Page Update

--- FrontEnd/src/Pages/LandingPage/Main.js
+++ FrontEnd/src/Pages/LandingPage/Main.js
@@ -4,6 +4,13 @@ import Carousel from "../../Components/LandingPage/Carousel";
 import data from "../../config/Data.json";
 import MakerkoGIF from "../../Components/LandingPage/MakerkoGIF";
 import { colors } from "../../Values/colors";
+import ProductionCapabilities from "../../Components/LandingPage/ProductionCapabilities";
+import productionType from "../../config/ProductionCapabilities.json";
+import AboutUs from "../../Components/LandingPage/AboutUs";
+import aboutUs from "../../config/AboutUs.json";
+import "./Main.css";
+import partners from "../../config/Partners.json";
+import PartnersAndCollaboration from "../../Components/LandingPage/PartnersAndCollaboration";
 
 function Main() {
     const slides = data;
@@ -12,7 +19,7 @@ function Main() {
             <div>
                 <Carousel slides={slides} />
                 <MakerkoGIF />
-                <div className="d-flex flex-column align-items-center justify-content-center pt-5 pb-5">
+                <div className="main-box-2">
                     <p
                         style={{
                             fontSize: "1.2rem",
@@ -31,10 +38,103 @@ function Main() {
                             color: colors.primary,
                             textDecoration: "underline",
                         }}
-                        href="#"
+                        href="https://www.google.com"
                     >
                         Learn How to Place Orders
                     </a>
+                </div>
+                <ProductionCapabilities productionType={productionType} />
+                <div className="main-box-4">
+                    <div className="main-sub-box-4">
+                        <div className="makers-col">
+                            <h2>Makers</h2>
+                            <p>
+                                Explore our manufacturing hubs to select from
+                                the portfolio of local makers. Users can sort
+                                and filter makers by their preferred company
+                                name, manufacturing process, material and
+                                location. Users know makers’ production
+                                capabilities, products, services, materials and
+                                rates by visiting their portfolio.
+                            </p>
+                            <button
+                                style={{
+                                    width: "200px",
+                                    height: "50px",
+                                    marginBottom: "30px",
+                                }}
+                            >
+                                Explores MAKERS
+                            </button>
+                            <a href="#">
+                                Are you a Maker? Sign Up to build your portfolio
+                            </a>
+                        </div>
+                        <div className="divider"></div>
+                        <div className="project-col">
+                            <h2>Projects</h2>
+                            <p>
+                                Share what you make. Someone might be in need of
+                                it. The Makerko platform provides makers and
+                                innovators greater visibility to their business
+                                and product ideas. Explore featured projects to
+                                learn how local innovators and makers are
+                                applying manufacturing technologies to bring
+                                good to humanity.
+                            </p>
+                            <button
+                                style={{
+                                    width: "200px",
+                                    height: "50px",
+                                    marginBottom: "30px",
+                                }}
+                            >
+                                View Projects
+                            </button>
+                            <a href="#">
+                                Share your innovation/ product/ project to
+                                feature in our platform.
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <AboutUs data={aboutUs} />
+                <div className="main-quote">
+                    <img
+                        style={{
+                            width: "100%",
+                            height: "400px",
+                            objectFit: "cover",
+                            opacity: "0.5",
+                        }}
+                        src="https://www.whatspaper.com/wp-content/uploads/2021/07/abstract-wallpaper-whatspaper.png"
+                        alt="quote"
+                    />
+                    <span
+                        style={{
+                            position: "absolute",
+                            width: "80%",
+                            fontSize: "2rem",
+                            textAlign: "center",
+                            color: colors.white,
+                        }}
+                    >
+                        “Creativity is allowing yourself to make mistakes. Art
+                        is knowing which ones to keep.” - Susan Tamrakar
+                    </span>
+                </div>
+                <PartnersAndCollaboration partners={partners} />
+                <MakerkoGIF />
+                <div>
+                    <img
+                        style={{
+                            width: "100%",
+                            height: "450px",
+                            objectFit: "cover",
+                        }}
+                        src="https://makerko.com/KnowledgeBank/Vacuum%20Forming%20.jpg"
+                        alt="image"
+                    />
                 </div>
             </div>
         </Section>
--- FrontEnd/src/Routing/Routing.js
+++ FrontEnd/src/Routing/Routing.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../Components/NavBar";
 import Main from "../Pages/LandingPage/Main";
+import FooterContainer from "../Components/Footer/Footer";
 
 function Routing() {
     return (
@@ -9,6 +10,7 @@ function Routing() {
             <Router>
                 <NavBar />
                 <Main />
+                <FooterContainer />
             </Router>
         </div>
     );
