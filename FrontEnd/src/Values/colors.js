5b87be1dae1e5d6915ff671728b081717e5e9778
aman0896 <aman.promech@gmail.com>
2021-11-26 12:12:00 +0545

    Update colors.js

--- FrontEnd/src/Values/colors.js
+++ FrontEnd/src/Values/colors.js
@@ -1,6 +1,11 @@
 export const colors = {
     white: "#FFFFFF",
-    black: "#000000",
+    dark: "#000000",
     primary: "#0000FF",
     secondary: "#C4C4C4",
+    success: "#28a745",
+    info: "#17a2b8",
+    warning: "#ffc107",
+    danger: "#dc3545",
+    light: "#f8f9fa",
 };
