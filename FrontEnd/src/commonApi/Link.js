b3ac5d460c4407cb66cfbf94624327ac80b99de1
aman0896 <aman.promech@gmail.com>
2021-11-29 15:25:49 +0545

    Update Link.js

--- FrontEnd/src/commonApi/Link.js
+++ FrontEnd/src/commonApi/Link.js
@@ -1,6 +1,16 @@
+import Axios from "axios";
+
 //file-upload-link
 export const File_Server = "https://images.makerko.com";
 
 // export const File_Server = "http://192.168.88.196:3000";
 
 // export const File_Server = "http://192.168.1.103:5000";
+
+//host api
+export const mainHost = "http://localhost/";
+
+export const hostApi = Axios.create({ baseURL: mainHost });
+
+//login
+export const login = "account/login";
