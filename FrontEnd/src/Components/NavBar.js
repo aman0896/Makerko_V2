e96840d990153d81141c0df8fbcf062a9923e6f9
aman0896 <aman.promech@gmail.com>
2021-11-29 15:26:59 +0545

    Update NavBar.js

--- FrontEnd/src/Components/NavBar.js
+++ FrontEnd/src/Components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { colors } from "../Values/colors";
+import { useHistory } from "react-router";
 import "./NavBar.css";
 import { FiUser } from "react-icons/fi";
 
@@ -41,8 +41,8 @@ function NavBar() {
                     </li>
                 </ul>
 
-                <div className="navbar-login">
-                    <FiUser />
+                <div>
+                    <ProfileAvatarLogin />
                 </div>
             </div>
         </nav>
@@ -50,3 +50,97 @@ function NavBar() {
 }
 
 export default NavBar;
+
+//If user is not Logged in
+function ProfileAvatarLogin() {
+    return (
+        <>
+            <div data-toggle="dropdown" className="dropdown navbar-login">
+                <FiUser />
+            </div>
+
+            <div className="dropdown-menu dropdown-menu-lg-right dropdown-menu-style">
+                <div>
+                    <a
+                        className="dropdown-item  dropdown-item-style"
+                        href="/login"
+                    >
+                        Sign In
+                    </a>
+                </div>
+                <div>
+                    <a
+                        className="dropdown-item dropdown-item-style"
+                        href="/register"
+                        type="button"
+                        data-toggle="modal"
+                        data-target="#signup"
+                    >
+                        {" "}
+                        Sign Up
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+//if user is alread logged in
+function ProfileAvatarLogout() {
+    const history = useHistory();
+    // const onclickLogout = () => {
+    //     axios.defaults.withCredentials = true;
+    //     axios.post(`${window.host}/logout`).then((response) => {
+    //         if (response.status === 202) {
+    //             console.log(response, "logout");
+    //             history.push({ pathname: "/" });
+    //             history.go();
+    //         }
+    //     });
+    // };
+
+    const onClickEditCustomerProfile = () => {
+        // history.push({ pathname: `/${userID}/customer-profile` });
+    };
+
+    const onClickEditProfileMaker = () => {
+        // history.push({ pathname: `/${userID}/manufacturer-profile` });
+    };
+    return (
+        <li className="dropdown nav-item">
+            <a data-toggle="dropdown">
+                <div className="icon-container">{"username.charAt(0)"}</div>
+            </a>
+
+            <ul className="dropdown-menu dropdown-menu-lg-right p-0">
+                <li>
+                    <a
+                        className="dropdown-item p-2 pl-4"
+                        // onClick={
+                        //     userType === "customer"
+                        //         ? onClickEditCustomerProfile
+                        //         : onClickEditProfileMaker
+                        // }
+                    >
+                        <FiUser size="22px" />{" "}
+                        <span className="ml-1">My Profile</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        className="dropdown-item p-2 pl-4 "
+                        // onClick={onclickLogout}
+                    >
+                        <i className="fas fa-sign-out-alt mr-2"></i> Log Out
+                    </a>
+                </li>
+            </ul>
+        </li>
+    );
+}
+
+// [
+//     { name: "Sign In", link: "/login" },
+//     { name: "Sign Up", link: "/signup" },
+// ]
