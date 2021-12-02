c864db87d4cf3f964402e60b717bb716b99b8ccd
aman0896 <aman.promech@gmail.com>
2021-12-02 13:41:22 +0545

    Update ReactToastify.js

--- FrontEnd/src/Components/ReactToastify.js
+++ FrontEnd/src/Components/ReactToastify.js
@@ -3,59 +3,94 @@ import { ToastContainer, Bounce, toast, Zoom, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { colors } from "../Values/colors";
 
-function ReactToastify({ closeTime = 2000, textColor, message, type }) {
-    switch (type) {
-        case "error":
-            toast.error(
-                <div>
-                    <i
-                        style={{ fontSize: "1rem" }}
-                        class="fas fa-exclamation-circle mr-2"
-                    />
-                    {message}
-                </div>
-            );
-            break;
-        case "info":
-            toast.info(message);
-            break;
-        case "success":
-            toast.success(
-                <div>
-                    <i
-                        style={{ fontSize: "1rem" }}
-                        class="fas fa-check-circle"
-                    ></i>
-                    {message}
-                </div>
-            );
-            break;
-        case "warning":
-            toast.warning(message);
-            break;
-        case "dark":
-            toast.dark(message);
-            break;
-        default:
-            break;
-    }
-
+function ReactToastify() {
     return (
         <Fragment>
             <ToastContainer
                 position="top-right"
-                autoClose={closeTime}
                 hideProgressBar={true}
                 newestOnTop={true}
                 closeOnClick
                 rtl={false}
                 pauseOnHover
                 transition={Slide}
-                toastStyle={{
-                    color: textColor ? textColor : colors.primary,
-                }}
             />
         </Fragment>
     );
 }
 export default ReactToastify;
+
+export const Toast = (message, type, closeTime, color) => {
+    switch (type) {
+        case "error":
+            toast.error(
+                <div>
+                    <i
+                        style={{ fontSize: "1rem" }}
+                        className="fas fa-exclamation-circle mr-2"
+                    />
+                    {message}
+                </div>,
+                {
+                    autoClose: closeTime,
+                    toastStyle: {
+                        color: color ? color : colors.primary,
+                    },
+                }
+            );
+            break;
+        case "info":
+            toast.info(
+                <div>
+                    <i
+                        style={{ fontSize: "1rem" }}
+                        className="fas fa-info-circle mr-2"
+                    />
+                    {message}
+                </div>,
+                {
+                    autoClose: closeTime,
+                    toastStyle: {
+                        color: color ? color : colors.primary,
+                    },
+                }
+            );
+            break;
+        case "success":
+            toast.success(
+                <div>
+                    <i
+                        style={{ fontSize: "1rem" }}
+                        className="fas fa-check-circle mr-2"
+                    ></i>
+                    {message}
+                </div>,
+                {
+                    autoClose: closeTime,
+                    toastStyle: {
+                        color: color ? color : colors.primary,
+                    },
+                }
+            );
+            break;
+        case "warning":
+            toast.warning(message, {
+                autoClose: closeTime,
+                toastStyle: {
+                    color: color ? color : colors.primary,
+                },
+            });
+            break;
+        case "dark":
+            toast.dark(message, {
+                autoClose: closeTime,
+                toastStyle: {
+                    color: color ? color : colors.primary,
+                },
+            });
+            break;
+        default:
+            toast(message);
+            break;
+    }
+};
