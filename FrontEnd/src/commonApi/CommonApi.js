da37439e9e57e92cd44ae1963668b52007ffd6f4
aman0896 <aman.promech@gmail.com>
2021-12-02 09:53:26 +0545

    Update CommonApi.js

--- FrontEnd/src/commonApi/CommonApi.js
+++ FrontEnd/src/commonApi/CommonApi.js
@@ -1,8 +1,24 @@
 import Axios from "axios";
+import { mainHost } from "./Link";
+
+Axios.defaults.withCredentials = true;
 
 export function getData(link, params, onSuccess, onFail) {
     Axios.get(mainHost + link, {
         params,
+        headers: { "content-type": "application/json" },
+    })
+        .then((res) => res)
+        .then((res) => {
+            onSuccess(res);
+        })
+        .catch((err) => {
+            onFail(err);
+        });
+}
+
+export function getDataWithNoParams(link, onSuccess, onFail) {
+    Axios.get(mainHost + link, {
         headers: { "content-type": "application/json" },
     })
         .then((res) => res)
