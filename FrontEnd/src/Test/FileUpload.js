c079df2b0eebca88f7fc99734696188ace971e69
aman0896 <aman.promech@gmail.com>
2021-12-03 22:29:03 +0545

    Update FileUpload.js

--- FrontEnd/src/Test/FileUpload.js
+++ FrontEnd/src/Test/FileUpload.js
@@ -1,10 +1,23 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { mainHost } from "../commonApi/Link";
 
 function FileUpload() {
     const [file, setFile] = useState();
     const [preview, setPreview] = useState();
+    const fileDir = "./public/uploads/customer/profile1638530203322.jpg";
+    const [imageSrc, setImageSrc] = useState();
+
+    useEffect(() => {
+        async function DownloadFile() {
+            const file = await FileDownload(fileDir, null);
+            console.log(file, "filedownload");
+            const url = window.URL.createObjectURL(new Blob([file]));
+            setImageSrc(url);
+        }
+
+        DownloadFile();
+    }, []);
 
     const onChangeFileUpload = (e) => {
         setFile(e.target.files[0]);
@@ -15,10 +28,10 @@ function FileUpload() {
     const onFileUpload = () => {
         console.log(file, "file");
         const formData = new FormData();
-        formData.append("file", file);
+        formData.append("profile", file);
 
         axios
-            .post(mainHost + "file", formData, {
+            .post(mainHost + "profile/customer", formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             })
             .then((response) => {
@@ -28,7 +41,13 @@ function FileUpload() {
 
     return (
         <div className="d-flex flex-column">
-            <input type="file" onChange={onChangeFileUpload} />
+            <input
+                id="file"
+                name="myImage"
+                type="file"
+                onChange={onChangeFileUpload}
+            />
+
             <img
                 style={{
                     height: "50%",
@@ -36,7 +55,7 @@ function FileUpload() {
                     margin: "20px 0px",
                     alignSelf: "center",
                 }}
-                src={preview}
+                src={preview ? preview : imageSrc}
                 alt="image"
             />
             <button
@@ -50,3 +69,39 @@ function FileUpload() {
 }
 
 export default FileUpload;
+
+//#region filedownolad Link Generate
+export const FileDownload = async (filePath) => {
+    var data = JSON.stringify({
+        filedir: filePath,
+    });
+
+    var config = {
+        method: "post",
+        responseType: "blob",
+        url: "http://localhost:3001/profile/download",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: data,
+    };
+
+    const fileData = await axios(config)
+        .then(function (response) {
+            console.log(response.data, "data");
+            return response.data;
+            // const url = window.URL.createObjectURL(new Blob([response.data]));
+            // const link = document.createElement("a");
+            // link.href = url;
+            // link.target = "_blank";
+            // link.setAttribute("download", fileName);
+            // document.body.appendChild(link);
+            // link.click();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    return fileData;
+};
+//#endregion
