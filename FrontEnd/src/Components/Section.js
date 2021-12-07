fa1e738d4cfabee977801c92ff022e1ff8c72918
aman0896 <aman.promech@gmail.com>
2021-12-07 10:44:35 +0545

    Delete Section.js

--- FrontEnd/src/Components/Section.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Section.css";
-
-function Section({ children }) {
-    return <div className="section-container">{children}</div>;
-}
-
-export default Section;
