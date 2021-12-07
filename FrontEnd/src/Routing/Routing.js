9d67740918b42dc6755963ff1c85d4671b4780ab
aman0896 <aman.promech@gmail.com>
2021-12-07 10:45:38 +0545

    Update Routing.js

--- FrontEnd/src/Routing/Routing.js
+++ FrontEnd/src/Routing/Routing.js
@@ -4,6 +4,7 @@ import FooterContainer from "../Components/Footer/FooterContainer";
 import NavBar from "../Components/NavBar";
 import ReactToastify from "../Components/ReactToastify";
 import CreateFeatureProject from "../Pages/FeatureProject/CreateFeatureProject";
+import GetAQuote from "../Pages/GetAQuote";
 import Main from "../Pages/LandingPage/Main";
 import Login from "../Pages/Login";
 import MakersSignup from "../Pages/MakersSignup";
@@ -12,33 +13,53 @@ import CustomerProfile from "../Pages/Profile/CustomerProfile";
 import Signup from "../Pages/Signup";
 import VerificationSuccess from "../Pages/VerificationSuccess";
 import FileUpload from "../Test/FileUpload";
-import CreateFeatureProject from "../Pages/FeatureProject/CreateFeatureProject";
 
 function Routing({ isAuth, currentUser, userType }) {
-  return (
-    <div>
-      <Router>
-        <NavBar isAuth={isAuth} currentUser={currentUser} userType={userType} />
-        <Switch>
-          {/* <Main /> */}
-          <Route exact path="/" component={Main} />
-          <Route exact path="/account/login" component={Login} />
-          <Route exact path="/account/signup" component={Signup} />
-          <Route exact path="/account/makers-signup" component={MakersSignup} />
-          <Route exact path="/account/verify" component={OTPVerification} />
-          <Route exact path="/verify-success" component={VerificationSuccess} />
-          <Route exact path="/new-project" component={CreateFeatureProject} />
-          <Route exact path="/file" component={FileUpload} />
-          <Route
-            exact
-            path="/account/edit-customerProfile"
-            component={CustomerProfile}
-          />
-        </Switch>
-      </Router>
-      <ReactToastify />
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <NavBar
+                    isAuth={isAuth}
+                    currentUser={currentUser}
+                    userType={userType}
+                />
+                <Switch>
+                    {/* <Main /> */}
+                    <Route exact path="/" component={Main} />
+                    <Route exact path="/account/login" component={Login} />
+                    <Route exact path="/account/signup" component={Signup} />
+                    <Route
+                        exact
+                        path="/account/makers-signup"
+                        component={MakersSignup}
+                    />
+                    <Route
+                        exact
+                        path="/account/verify"
+                        component={OTPVerification}
+                    />
+                    <Route
+                        exact
+                        path="/verify-success"
+                        component={VerificationSuccess}
+                    />
+                    <Route
+                        exact
+                        path="/new-project"
+                        component={CreateFeatureProject}
+                    />
+                    <Route exact path="/file" component={FileUpload} />
+                    <Route
+                        exact
+                        path="/profile/customer"
+                        component={CustomerProfile}
+                    />
+                    <Route exact path="/get-quote" component={GetAQuote} />
+                </Switch>
+            </Router>
+            <ReactToastify />
+        </div>
+    );
 }
 
 export default Routing;
