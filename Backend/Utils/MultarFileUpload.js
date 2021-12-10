989d4d3ffacbc793f8263c37d99b9bae98078329
aman0896 <aman.promech@gmail.com>
2021-12-10 15:18:05 +0545

    Update MultarFileUpload.js

--- Backend/Utils/MultarFileUpload.js
+++ Backend/Utils/MultarFileUpload.js
@@ -3,6 +3,7 @@ var multer = require("multer");
 const path = require("path");
 const projectPath = path.dirname(process.cwd());
 
+//single file uploads
 function SingleFileUpload(fieldName, filePath) {
     var storage = multer.diskStorage({
         destination: function (req, file, cb) {
@@ -19,6 +20,7 @@ function SingleFileUpload(fieldName, filePath) {
     return upload;
 }
 
+//multiple file uploads
 function MultipleFileUpload(fieldName, path) {
     var storage = multer.diskStorage({
         destination: function (req, file, cb) {
@@ -32,6 +34,23 @@ function MultipleFileUpload(fieldName, path) {
         },
     });
     const upload = multer({ storage: storage }).array(fieldName);
+    return upload;
+}
+
+//multiple field uploads
+function MultipleFieldUpload(fieldData) {
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "./public/temp/");
+        },
+        filename: function (req, file, cb) {
+            cb(
+                null,
+                file.fieldname + Date.now() + path.extname(file.originalname)
+            );
+        },
+    });
+    const upload = multer({ storage: storage }).fields(fieldData);
     return upload;
 }
 
