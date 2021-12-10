11f13119a55dfc155ea305328b55fa9b53ab620b
aman0896 <aman.promech@gmail.com>
2021-12-10 15:17:39 +0545

    Update Profile.js

--- Backend/routes/Profile.js
+++ Backend/routes/Profile.js
@@ -5,55 +5,52 @@ const projectPath = path.dirname(process.cwd());
 var fs = require("fs");
 const { SingleFileUpload } = require("../Utils/MultarFileUpload");
 const FileDownload = require("../Utils/FileDownload");
+const { FileMove } = require("../Utils/Operations");
+const { DBQuery } = require("../DBController/DatabaseQuery");
 const router = express.Router();
 
-router.post("/customer", (req, res) => {
-    console.log("profile check");
-    const upload = SingleFileUpload("profile", null);
-    const dir = "./public/uploads/customer/";
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-    upload(req, res, (err) => {
+router.post("/customer-edit", (req, res) => {
+    const upload = SingleFileUpload("profileImage", null);
+    upload(req, res, async (err) => {
+        const imageFile = req.file;
+        const userDetails = JSON.parse(req.body.currentUser);
+        const userUpdateDetails = JSON.parse(req.body.userUpdates);
+        console.log(imageFile, userDetails, userUpdateDetails, "details");
+        const dir = `./public/uploads/customer/${userDetails.Customer_ID}/${imageFile.fieldname}/`;
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
         if (err) {
             console.log(err, "error");
         } else {
-            let tmp_path = req.file.path;
-            let target_path = dir + req.file.filename;
-
-            /** A better way to copy the uploaded file. **/
-            let src = fs.createReadStream(tmp_path);
-            let dest = fs.createWriteStream(target_path);
-            src.pipe(dest);
-            src.on("end", function () {
-                console.log("complete");
-                req.file.path = target_path;
-                req.file.destination = dir;
+            let tmp_path = imageFile.path;
+            let target_path = dir + imageFile.filename;
+            const filePath = await FileMove(tmp_path, target_path);
+            const sqlQuery =
+                "UPDATE customer SET First_Name =?, Last_Name=?, Email=?, Phone_Number=?, Address=?, Profile_Image = ? WHERE Customer_ID = ?";
+            const data = [
+                userUpdateDetails.firstName,
+                userUpdateDetails.lastName,
+                userUpdateDetails.email,
+                userUpdateDetails.phoneNumber,
+                userUpdateDetails.address,
+                JSON.stringify({
+                    filename: imageFile.filename,
+                    filePath: filePath,
+                }),
+                userDetails.Customer_ID,
+            ];
+            DBQuery(sqlQuery, data, (err, result) => {
+                if (err) {
+                    return console.log(err, "update customer profile error");
+                } else {
+                    res.json(result);
+                    return;
+                }
             });
-            src.on("error", function (err) {
-                console.log("error");
-            });
-            src.on("close", function (err) {
-                fs.unlink(tmp_path, function (err) {
-                    console.log(err);
-                });
-            });
-
-            res.json(req.file);
         }
     });
 });
-
-// Start download any File or images
-router.post("/download", async function (req, res) {
-    const path = req.body.filedir;
-    const filedir = `${path}`;
-    const file = await FileDownload(filedir);
-    if (file) {
-        res.download(file); // Set disposition and send it.
-        return;
-    }
-});
-// End download any File or images
 
 module.exports = router;
