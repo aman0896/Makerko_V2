8a94ddbd41896176854a87fd09e6ce8affdb8687
aman0896 <aman.promech@gmail.com>
2021-12-15 13:49:20 +0545

    Update Profile.js

--- Backend/routes/Profile.js
+++ Backend/routes/Profile.js
@@ -63,127 +63,72 @@ router.post("/customer-edit", (req, res) => {
 });
 
 router.post("/maker-edit", (req, res) => {
-    const upload = SingleFileUpload("profileImage", null);
-    upload(req, res, async (err) => {
-        console.log(req.body, "___________________________");
-        var imageFile = req.file;
-        const prevImage =
-            req.body.prevImage !== "undefined"
-                ? JSON.parse(req.body.prevImage)
-                : null;
-        const userDetails = JSON.parse(req.body.currentUser);
-        const userUpdateDetails = JSON.parse(req.body.userUpdates);
-        console.log(imageFile, userDetails, userUpdateDetails, "details");
-        console.log(req.file, "file line 71");
-        if (req.file) {
-            console.log("inside if line 73");
-            var dir = `./public/uploads/maker/${userDetails.Manufacturer_ID}/${imageFile.fieldname}/`;
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir, { recursive: true });
-            }
-            FileDelete(prevImage.filePath);
-        } else {
-            console.log("line 79");
-            console.log(prevImage, "line 79");
-            imageFile = prevImage;
-        }
-
-        if (err) {
-            console.log(err, "error");
-        } else {
-            let tmp_path = imageFile.path;
-            let target_path = dir + imageFile.filename;
-            const filePath = await FileMove(tmp_path, target_path);
-            console.log(userUpdateDetails, "user");
-            const sqlQuery =
-                "UPDATE manufacturer SET Company_Name=?, Logo=?, Contact_Person=?,Email=?,Phone_Number=?, Address = ?, Brief_Description = ?, Additional_Details = ? WHERE Manufacturer_ID = ?";
-            const data = [
-                userUpdateDetails.companyName,
-                JSON.stringify({
-                    filename: imageFile.filename,
-                    filePath: req.file ? filePath : imageFile.filePath,
-                }),
-                userUpdateDetails.contactPerson,
-                userUpdateDetails.email,
-                userUpdateDetails.phoneNumber,
-                userUpdateDetails.address,
-                userUpdateDetails.brief_description,
-                userUpdateDetails.additional_details,
-
-                userDetails.Manufacturer_ID,
-            ];
-            DBQuery(sqlQuery, data, (err, result) => {
-                if (err) {
-                    return console.log(err, "update maker profile error");
-                } else {
-                    res.json(result);
-                    return;
+    try {
+        const upload = SingleFileUpload("profileImage", null);
+        upload(req, res, async (err) => {
+            console.log(req.body, "___________________________");
+            var imageFile = req.file;
+            console.log(req.file, "files___________");
+            const prevImage =
+                req.body.prevImage !== "undefined"
+                    ? JSON.parse(req.body.prevImage)
+                    : null;
+            const userDetails = JSON.parse(req.body.currentUser);
+            const userUpdateDetails = JSON.parse(req.body.userUpdates);
+            console.log(imageFile, userDetails, userUpdateDetails, "details");
+            console.log(req.file, "file line 71");
+            if (req.file) {
+                console.log("inside if line 73");
+                var dir = `./public/uploads/maker/${userDetails.Manufacturer_ID}/${imageFile.fieldname}/`;
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, { recursive: true });
                 }
-            });
-        }
-    });
-    router.post("/multiple-upload", async (req, res) => {
-        console.log("profile check");
-        const upload = MultipleFileUpload("profile", null);
-        const dir = "./public/uploads/multipleUpload/";
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
+                if (prevImage && prevImage.filePath) {
+                    FileDelete(prevImage.filePath);
+                }
+            } else {
+                console.log("line 79");
+                console.log(prevImage, "line 79");
+                imageFile = prevImage;
+            }
 
-        upload(req, res, (err) => {
             if (err) {
                 console.log(err, "error");
             } else {
-                console.log(req.files, "req");
-                const files = req.files;
-                for (let i = 0; i < files.length; i++) {
-                    let tmp_path = files[i].path;
-                    console.log(tmp_path, "path");
-                    let target_path =
-                        "./public/uploads/multipleUpload/" + files[i].filename;
-                    let src = fs.createReadStream(tmp_path);
-                    let dest = fs.createWriteStream(target_path);
-                    src.pipe(dest);
-                    src.on("end", function () {
-                        console.log("complete");
-                    });
-                    src.on("error", function (err) {
-                        console.log("error");
-                    });
-                    src.on("close", function (err) {
-                        console.log(tmp_path, "path");
-                        fs.unlink(tmp_path, function (err) {
-                            console.log(err);
-                        });
-                    });
-                }
+                let tmp_path = imageFile.path;
+                let target_path = dir + imageFile.filename;
+                const filePath = await FileMove(tmp_path, target_path);
+                console.log(userUpdateDetails, "user");
+                const sqlQuery =
+                    "UPDATE manufacturer SET Company_Name=?, Logo=?, Contact_Person=?,Email=?,Phone_Number=?, Address = ?, Brief_Description = ?, Additional_Details = ? WHERE Manufacturer_ID = ?";
+                const data = [
+                    userUpdateDetails.companyName,
+                    JSON.stringify({
+                        filename: imageFile.filename,
+                        filePath: req.file ? filePath : imageFile.filePath,
+                    }),
+                    userUpdateDetails.contactPerson,
+                    userUpdateDetails.email,
+                    userUpdateDetails.phoneNumber,
+                    userUpdateDetails.address,
+                    userUpdateDetails.brief_description,
+                    userUpdateDetails.additional_details,
 
-                //onsole.log(req.file.path, "pathhhh");
-                //   var tmp_path = req.file.path;
-                //   var target_path =
-                //     "./public/uploads/customer/" +
-                //     req.file.fieldname +
-                //     Date.now() +
-                //     path.extname(req.file.originalname);
-
-                //   /** A better way to copy the uploaded file. **/
-                //   var src = fs.createReadStream(tmp_path);
-                //   var dest = fs.createWriteStream(target_path);
-                //   src.pipe(dest);
-                //   src.on("end", function () {
-                //     console.log("complete");
-                //   });
-                //   src.on("error", function (err) {
-                //     console.log("error");
-                //   });
-                //   src.on("close", function (err) {
-                //     fs.unlink(tmp_path, function (err) {
-                //       console.log(err);
-                //     });
-                //   });
+                    userDetails.Manufacturer_ID,
+                ];
+                DBQuery(sqlQuery, data, (err, result) => {
+                    if (err) {
+                        return console.log(err, "update maker profile error");
+                    } else {
+                        res.json(result);
+                        return;
+                    }
+                });
             }
         });
-    });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.get("/maker-get-map", async (req, res) => {
