01a538b05f2db255e5d86659b924d1791e22b737
aman0896 <aman.promech@gmail.com>
2021-12-15 13:09:01 +0545

    Update ProductionCapabilities.js

--- FrontEnd/src/Components/LandingPage/ProductionCapabilities.js
+++ FrontEnd/src/Components/LandingPage/ProductionCapabilities.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductionCapabilities.css";
 import SlideView from "../slideView/SlideView";
 import { colors } from "../../Values/colors";
 import { Button2 } from "../Button";
+import ModalChoice from "../modal/ModalChoice";
+import { useHistory } from "react-router-dom";
 
 function ProductionCapabilities({ productionType }) {
+    const history = useHistory();
+    const [showModal, setShowModal] = useState(false);
+
     const imageStyle = {
         objectFit: "cover",
         width: "100%",
@@ -26,6 +31,22 @@ function ProductionCapabilities({ productionType }) {
         width: "98%",
     };
 
+    const showModalChoice = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    const onClickHaveDesign = () => {
+        window.location.href = "/get-quote";
+    };
+
+    const onClickRequestDesign = () => {
+        window.location.href = "/request-design";
+    };
+
     return (
         <div className="main-container">
             <div className="sub-container">
@@ -45,12 +66,24 @@ function ProductionCapabilities({ productionType }) {
                         Die Making / Casting / Metal Fabrication / Wood Working
                     </span>
                     <span>
-                        <Button2 buttonSize="button2--large">
+                        <Button2
+                            onClick={showModalChoice}
+                            buttonSize="button2--large"
+                        >
                             Get a Quote
                         </Button2>
                     </span>
                 </div>
             </div>
+            <ModalChoice
+                show={showModal}
+                handleClose={handleClose}
+                onClickButton1={onClickHaveDesign}
+                onClickButton2={onClickRequestDesign}
+                title="Get a quote"
+                btnTitle1="Have a Design"
+                btnTitle2="Request a Desgin"
+            />
         </div>
     );
 }
