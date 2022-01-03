9781bdcff81fb2ae10985d0701ec4de87a8a6da8
aman0896 <82374088+aman0896@users.noreply.github.com>
2022-01-03 16:13:03 +0545

    Update Projects.js

--- FrontEnd/src/Pages/FeatureProject/Projects.js
+++ FrontEnd/src/Pages/FeatureProject/Projects.js
@@ -1,60 +1,171 @@
-// import React, { useEffect, useState } from "react";
-// import { useHistory } from "react-router-dom";
-// import CardViewVerticalComponent from "../../Components/card/CardViewVerticalComponent";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import CardViewVerticalComponent from "../../Components/card/CardViewVerticalComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { FeatureProjectList } from "../../Components/Redux/Actions/FeatureProjectList";
+import ReactToHtml from "react-html-parser";
 
-// const project = [
-//     {
-//         name: "project name",
-//         description:
-//             "At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.",
-//         image: "https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg",
-//     },
-//     {
-//         name: "project name",
-//         description:
-//             "At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.",
-//         image: "https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg",
-//     },
-//     {
-//         name: "project name",
-//         description:
-//             "At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.",
-//         image: "https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg",
-//     },
-//     {
-//         name: "project name",
-//         description:
-//             "At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.",
-//         image: "https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg",
-//     },
-// ];
+function Projects() {
+    const dispatch = useDispatch();
+    const projectList = useSelector((state) => state.projectList.projectList);
 
-// function Projects() {
+    const [projects, setProjects] = useState(null);
+
+    useEffect(() => {
+        FeatureProjectList(dispatch);
+        // setProjects(project);
+    }, []);
+
+    useEffect(() => {
+        if (projectList) {
+            console.log(projectList, "projectList");
+            setProjects(projectList);
+        }
+    }, [projectList]);
+
+    const onProjectClick = (project) => {
+        window.open(
+            `/projects/${project.Project_ID}/${project.Title}`,
+            "_blank"
+        );
+    };
+
+    const showProjects =
+        projects &&
+        projects.map((project, index) => {
+            return (
+                <CardViewVerticalComponent
+                    index={index}
+                    name={project.Title}
+                    image={project.Cover_Image}
+                    data={project}
+                    description={ReactToHtml(project.Summary)}
+                    onPress={onProjectClick}
+                    // imageFit="contain"
+                    // selectedCard={(selectedCard) => {
+                    //     // setSelectedHub(selectedCard);
+                    // }}
+                />
+            );
+        });
+
+    return (
+        <>
+            <div className="mainComponent">
+                <div className="d-flex justify-content-between align-items-center flex-wrap">
+                    <span
+                        className="heading text-uppercase"
+                        style={{ fontSize: "36px", color: "black" }}
+                    >
+                        PROJECTS
+                    </span>
+                    <form>
+                        <div class="input-group">
+                            <div class="form-outline">
+                                <input
+                                    type="search"
+                                    placeholder="Search by Hubs, Location ,Process ,Material"
+                                    class="form-control"
+                                    style={{ width: "300px", fontSize: "12px" }}
+                                />
+                            </div>
+                            <button
+                                type="button"
+                                class="btn btn-white"
+                                style={{
+                                    position: "absolute",
+                                    right: "0",
+                                }}
+                            >
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div className="row justify-content-around mt-4">
+                    {showProjects}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Projects;
+
+// function MakersHub() {
 //     const history = useHistory();
-//     const [projects, setProjects] = useState(null);
+//     const [selectedHub, setSelectedHub] = useState();
+//     const [hubs, setHubs] = useState(null);
+//     const makersList = useSelector((state) => state.makersList.makersList);
 
 //     useEffect(() => {
-//         setProjects(project);
-//     }, []);
+//         if (makersList) setHubs(makersList);
+//     }, [makersList]);
 
-//     const handleClick = (event) => {
-//         let clickedHub = event.target.value;
+//     const handleClick = (data) => {
+//         let clickedHub = data;
+//         console.log(clickedHub, "Hub");
 //         // history.push({
 //         //     pathname: "/makers/details",
 //         //     state: { clickedHub },
 //         // });
 //     };
+
+//     const hubList =
+//         hubs &&
+//         hubs.map((hub, index) => {
+//             return (
+//                 <CardViewVerticalComponent
+//                     index={index}
+//                     name={hub.Company_Name}
+//                     image={hub.Logo}
+//                     data={hub}
+//                     description={hub.Brief_Description}
+//                     // imageFit="contain"
+//                     selectedCard={(selectedCard) => {
+//                         setSelectedHub(selectedCard);
+//                     }}
+//                 />
+//             );
+//         });
+
 //     return (
 //         <>
-//             {projects && (
-//                 <CardViewVerticalComponent
-//                     header="projects hub"
-//                     data={projects}
-//                     handleClick={handleClick}
-//                 />
-//             )}
+//             <div className="mainComponent">
+//                 <div className="d-flex justify-content-between align-items-center flex-wrap">
+//                     <span
+//                         className="heading text-uppercase"
+//                         style={{ fontSize: "36px", color: "black" }}
+//                     >
+//                         MAKERS HUB
+//                     </span>
+//                     <form>
+//                         <div class="input-group">
+//                             <div class="form-outline">
+//                                 <input
+//                                     type="search"
+//                                     placeholder="Search by Hubs ,Location ,Process ,Material"
+//                                     class="form-control"
+//                                     style={{ width: "300px", fontSize: "12px" }}
+//                                 />
+//                             </div>
+//                             <button
+//                                 type="button"
+//                                 class="btn btn-white"
+//                                 style={{
+//                                     position: "absolute",
+//                                     right: "0",
+//                                 }}
+//                             >
+//                                 <i class="fas fa-search"></i>
+//                             </button>
+//                         </div>
+//                     </form>
+//                 </div>
+//                 <div className="row justify-content-around mt-4">{hubList}</div>
+//             </div>
 //         </>
 //     );
 // }
 
-// export default Projects;
+// export default MakersHub;
