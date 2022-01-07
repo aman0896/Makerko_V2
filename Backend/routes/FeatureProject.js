eb20e297304917834bb1d44273be94212205dbf3
aman0896 <82374088+aman0896@users.noreply.github.com>
2022-01-07 15:16:35 +0545

    Update FeatureProject.js

--- Backend/routes/FeatureProject.js
+++ Backend/routes/FeatureProject.js
@@ -8,11 +8,18 @@ router.post("/create", (req, res) => {
 });
 
 //get all feature_project from db
+// router.get("/get-featureproject-list", async (req, res) => {
+//     const sqlQuery = "SELECT * FROM feature_project";
+//     const data = await DBQuery2(sqlQuery);
+//     console.log(data, "data");
+//     res.json(data);
+// });
+
+//get all feature_project from db
 router.get("/get-featureproject-list", async (req, res) => {
-    const sqlQuery = "SELECT * FROM feature_project";
+    const sqlQuery = "SELECT * FROM project";
     const data = await DBQuery2(sqlQuery);
-    console.log(data, "data");
+    console.log(JSON.parse(data[0].Content)[0], "data");
     res.json(data);
 });
-
 module.exports = router;
