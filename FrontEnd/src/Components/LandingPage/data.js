9ffa690b03ae64d55c33b423a5d80dd1d9e1c052
aman0896 <82374088+aman0896@users.noreply.github.com>
2022-02-07 14:17:32 +0545

    Create data.js

---
+++ FrontEnd/src/Components/LandingPage/data.js
@@ -0,0 +1,8 @@
+export const slideData = [
+    {
+        image: "./assests/sliderimage/makerko_challenge_banner.png",
+    },
+    {
+        image: "./assests/sliderimage/Maker_ko_sustanibility_model.png",
+    },
+];
